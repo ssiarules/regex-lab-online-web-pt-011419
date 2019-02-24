395751d0e04912024534287afb1e3bc356afbafd
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-24 03:05:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,10 @@ end
 
 def words_five_letters_long(text)
   text.scan(/\b[a-z]{5}\b/i)
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(//) ? true : false 
 end
 
 def valid_phone_number?(phone)
